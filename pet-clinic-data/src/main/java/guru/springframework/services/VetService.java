5ec615b5cf882d30d1e9f0bcf71c7cb257c0d5b0
chiara.galatro <chiara.galatro@we-plus.eu>
2022-12-06 12:38:05 +0100

    implement interfaces for services closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/VetService.java
@@ -0,0 +1,12 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Pet;
+import guru.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
